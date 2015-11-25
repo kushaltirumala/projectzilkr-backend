20a22cbdfdd1914b0533f6a5fcc0bbe20fae27fc
Kushal Tirumala <kushaltirumala99@gmail.com>
2015-11-25 12:29:05 -0800

    ye

--- server.js
+++ server.js
@@ -38,4 +38,4 @@ require('./app/routes.js')(app, passport); // load our routes and pass in our ap
 
 // launch ======================================================================
 app.listen(port);
-console.log('The magic happens on port ' + port);
+console.log('server running ' + port);
